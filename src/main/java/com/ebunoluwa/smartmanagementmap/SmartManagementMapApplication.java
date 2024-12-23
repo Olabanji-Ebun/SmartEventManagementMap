3ac16513c6ad86008babc3cae201f7fc73447bd1
Olabanji-Ebun <ayooluwaebun8@gmail.com>
2024-12-23 17:00:29 +0100

    First commit

---
+++ src/main/java/com/ebunoluwa/smartmanagementmap/SmartManagementMapApplication.java
@@ -0,0 +1,13 @@
+package com.ebunoluwa.smartmanagementmap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartManagementMapApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SmartManagementMapApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ebunoluwa/smartmanagementmap/SmartManagementMapApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ebunoluwa.smartmanagementmap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartManagementMapApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
